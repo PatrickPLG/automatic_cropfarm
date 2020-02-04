f30feb07db133dbc69fe5eab385ae4674dba79df
PatrickPLG <44425816+PatrickPLG@users.noreply.github.com>
2020-02-04 13:46:37 +0100

    Update automatic_cropfarm.ino

--- automatic_cropfarm/automatic_cropfarm.ino
+++ automatic_cropfarm/automatic_cropfarm.ino
@@ -1,21 +1,18 @@
 /*
  * Automatic cropfarm for small gardens
  * Made by PatrickPLG og Klint02
- * version 1.0 Public Release
+ * version 1.1 Public Release
  */
 
+// Include libraries
 #include <Adafruit_Sensor.h>
-
 #include <DHT.h>
 #include <DHT_U.h>
-
-#include  <Wire.h> // include wire
+#include  <Wire.h>
 #include <LiquidCrystal.h>
-//#include <Average.h>
+// ------------------
 
-
-// Noter til opbygning
-// Skærm
+// Notes to setup for screen
 /*
   VSS = Højre på Potentiometer
   VDD = +
@@ -34,102 +31,115 @@
   Venste = +
 
 */
-// Henter bibloteket LiquidCrystal som bruges til lcd skærmen og så hvilke pins ledninger sider i
+// ------------------
+
+// Which pins the LCD screen uses / Setup for LCD screen
 LiquidCrystal lcd(32, 30, 28, 26, 24, 22);
+// ------------------
 
-//Temeperatur sensor
+// Temeperature sensor
 DHT dht(A1, DHT11);
+// ------------------
 
-
-// Target skærm
+// Target values
 int tempTarget = 25;
 int humTarget = 700;
 int waterTarget = 7;
 
-//------------------
+// ------------------
 
-// Actual skærm
+// Actual values
 int sekDelay = 5;
 int humActual = 500;
 
-//------------------
+// ------------------
 
-int value;
+// Different sensors
 int temp = A1;
 #define SensorPin A0
 float sensorValue = A0;
+int transistor = transistor;
+// ------------------
 
+// Values to the settings menu
 int menu = 1;
-
 void updateMenu();
+// ------------------
 
+// Inputs that the bluetooth module and app uses
 char state = 0;
-
 char state2 = 0;
+// ------------------
 
+
+// Different booleans there is used for making different things on the screen appear
 boolean check = false;
-
 boolean updateMenuRUN = false;
-
-boolean he = false;
+boolean runSettings = false;
+boolean sensorValueRUN = true;
+// ------------------
 
 void setup() {
+  // Starts Serial
   Serial.begin(9600);
-  pinMode(34, OUTPUT);
+  // Sets the transistor to OUTPUT 
+  pinMode(transistor, OUTPUT);
+  // Starts the DHT sensor
   dht.begin();
+  // Starts the LCD screen
   lcd.begin(20, 4);
-  //updateMenu();
 }
 
 void loop() {
   if (check == false) {
-    // Jord-fugtigheds sensor
-    value = digitalRead(A0);
-
-    // Hvis jord-fugtigheds sensoren er high
-    if (value == HIGH) {
-      // Transistoren sættes til high = pumpen kører
-      digitalWrite(34, HIGH);
+    // Soil Moisture Sensor and pump
+    // If the Soil Moisture Sensors input is high
+    if (sensorValue == HIGH) {
+      // Set the input to the Transistor high which starts the pump
+      digitalWrite(transistor, HIGH);
+      // Delay
       delay(waterTarget * 1000);
-      digitalWrite(34, LOW);
+      // Stops the pump
+      digitalWrite(transistor, LOW);
     }
 
-    // Hvis jord-fugtigheds sensoren er low
-    if (value == LOW)
+    // If the Soil Moisture Sensor is low
+    if (sensorValue == LOW)
     {
-      // Transistoren sættes til low = intet vand i pumpen
-      digitalWrite(34, LOW);
+      // Set the input to the Transistor low which stops the pump
+      digitalWrite(transistor, LOW);
     }
+    // ------------------
 
-    // Delay som opdatere skærmen
+    // Delay
     delay(1000);
 
-    // Læser temperaturen i celsius
+    // Temeperature
+    // Reads the temeperature in celsius
     float t = dht.readTemperature();
-    // læser temeperaturen i Fahrenheit
+    // Reads the temeperature in Fahrenheit
     float f = dht.readTemperature(true);
+    // ------------------
 
-    // Tjekker om der er fejl med temeperatur sensoren
-    if (isnan(t) || isnan(f)) {
-      //Serial.println("Failed to read from DHT sensor!");
-      return;
-    }
 
-    // Kører 100 målinger
-    for (int i = 0; i <= 100; i++)
-    {
-      //
+    // Soil Moisture Sensor
+    // Runs measurement of the Soil Moisture Sensor while sensorValueRUN is true
+    while sensorValueRUN = true {
       sensorValue = sensorValue + analogRead(SensorPin);
       delay(1);
     }
+
+    // Takes the value of sensorValue and divides it by 100
     sensorValue = sensorValue / 100.0;
-    //Serial.println(sensorValue);
+    // ------------------
+
+    // delay
     delay(30);
 
-
-    // Sætter cursoren i 0,1
+    // LCD Screen
+    // Places the cursor in 0,1
     lcd.setCursor(0, 1);
-    // Printer temperaturen i celsius og Fahrenheit
+    // Prints the temperature in celsius and Fahrenheit
     lcd.print("Temp: ");
     lcd.print(t);
     lcd.print((char)223);
@@ -137,72 +147,102 @@ void loop() {
     lcd.print(f);
     lcd.print("F");
 
-    // Sætter cursoren i 0,2
+    // Places the cursor in 0,2
     lcd.setCursor(0, 2);
-
-    // Printer Jord fugtigheden
+    // Prins value from the Soil Moisture Sensor
     lcd.print("Fugtighed: ");
     lcd.print(sensorValue);
     lcd.print("");
-
-
+    // ------------------
   }
+  // initiate settings menu
+  // Checks if there is coming data from the bluetooth module
   if (Serial.available() > 0) {
     state = Serial.read();
+    // if input from app to bluetoth module is equal to 2
     if (state == '2') {
-      he = false;
+      // sets runSettings to false
+      runSettings = false;
+      // sets check to true
       check = true;
+      // clear the LCD screen
       lcd.clear();
-      while (he == false) {
+      // while runsettings is equal to false, run settings
+      while (runSettings == false) {
         settings();
       }
     }
   }
+  // ------------------
 }
 
 void settings() {
+  // updateMenu
+  // runs updateMenu one time
   if (updateMenuRUN == false) {
     updateMenu();
     updateMenuRUN = true;
   }
+  // ------------------
+
+  // navigation in settings, with data from app
+  // Checks if there is coming data from the bluetooth module
   if (Serial.available() > 0) {
     state = Serial.read();
+    // if input from app to bluetoth module is equal to a
     if (state == 'a') {
-      //digitalWrite(13, HIGH);
+      // prints "down" in the app
       Serial.println("Down");
+      // moves cursor in menu up
       menu++;
+      // updates the menu, so it shows
       updateMenu();
+      // delay
       delay(100);
-      //while (results.value == 0XFF30CF);
     }
+    // if input from app to bluetoth module is equal to b
     if (state == 'b') {
-      //digitalWrite(13, HIGH);
+      // prints "up" in the app
       Serial.println("Up");
+      // moves cursor down in menu
       menu--;
+      // updates the menu, so it shows
       updateMenu();
+      // delay
       delay(100);
-      //while (results.value == 0XFF30CF);
     }
+    // if input from app to bluetoth module is equal to 0
     if (state == '0') {
+      // prints "+" in the app
       Serial.println("+");
+      // executes the action which the cursor is on
       executeAction();
+      // updates the menu
       updateMenu();
+      // delay
       delay(100);
-      //while (results.value == 0XFF629D);
     }
+    // if input from app to bluetoth module is equal to 1
     if (state == '1') {
+      // prints "-" in the app
       Serial.println("-");
+      // executes action2
       executeAction2();
+      // updates the menu
       updateMenu();
+      // delay
       delay(100);
-      //while (results.value == 0XFF629D);
     }
   }
+  // ------------------
 }
 
 
 void updateMenu() {
-  if (he == false) {
+  // Visual navigation
+  // sets runsettings to false
+  if (runSettings == false) {
+    // runs different visuals on the LCD screen with input from the app
     switch (menu) {
       case 0:
         menu = 1;
@@ -288,9 +328,12 @@ void updateMenu() {
         break;
     }
   }
+  // ------------------
 }
 
 void executeAction() {
+  // execute action
+  // executes the action which the cursor is on
   switch (menu) {
     case 1:
       action1();
@@ -299,34 +342,49 @@ void executeAction() {
       action2();
       break;
     case 3:
-      action3(); //Måske while løkke med state = 1
+      action3();
       break;
     case 4:
       action4();
       break;
   }
+  // ------------------
 }
 
+// actions related to executeAction
 void action1() {
+  // plus tempTarget
   tempTarget++;
 }
 void action2() {
+  // plus humTarget
   humTarget++;
 }
 void action3() {
+  // plus waterTarget
   waterTarget++;
 }
 void action4() {
+  // clears the LCD screen
   lcd.clear();
+  // prints "Quitting" on the LCD screen
   lcd.print(">Quitting");
+  // delay
   delay(1500);
+  // clears the LCD screen
   lcd.clear();
-  he = true;
+  // sets runsettings to true
+  runSettings = true;
+  // sets check to false
   check = false;
+  // sets sekDelay equal to waterTarget
   sekDelay = waterTarget;
 }
+// ------------------
 
 void executeAction2() {
+  // execute action2
+  // executes the action which the cursor is on
   switch (menu) {
     case 1:
       action5();
@@ -335,29 +393,42 @@ void executeAction2() {
       action6();
       break;
     case 3:
-      action7(); //Måske while løkke med state = 1
+      action7();
       break;
     case 4:
       action8();
       break;
   }
+  // ------------------
 }
 
+// actions related to executeAction2
 void action5() {
+  // minus tempTarget
   tempTarget--;
 }
 void action6() {
+  // minus humTarget
   humTarget--;
 }
 void action7() {
+  // minus waterTarget
   waterTarget--;
 }
 void action8() {
+  // clears the LCD screen
   lcd.clear();
+  // prints "Quitting" on the lcd screen
   lcd.print(">Quitting");
+  // delay
   delay(1500);
+  // clears the LCD screen
   lcd.clear();
-  he = true;
+  // sets runsettings to true
+  runSettings = true;
+  // sets check to false
   check = false;
+  // sets sekDelay equal to waterTarget
   sekDelay = waterTarget;
 }
+// ------------------
